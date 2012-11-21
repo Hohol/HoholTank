f40b12d48407578672881110e6d14e92bfa42ae8
Hohol <nikita.glashenko@gmail.com>
2012-11-21 16:14:32 +0300

    Optimized "Inside" function

--- ActualStrategy.cs
+++ ActualStrategy.cs
@@ -761,12 +761,16 @@ abstract class ActualStrategy
 
 	static bool Inside(MutableUnit unit, double x, double y, double precision)
 	{
+		double w = unit.Width / 2 + precision;
+		double h = unit.Height / 2 + precision;
+
+		if (x < unit.X - w || x > unit.X + w ||  // w must be greater than h
+		   y < unit.Y - w || y > unit.Y + w)
+			return false;
 		double d = unit.GetDistanceTo(x, y);
 		double angle = unit.GetAngleTo(x, y);
 		x = d * Math.Cos(angle);
-		y = d * Math.Sin(angle);
-		double w = unit.Width / 2 + precision;
-		double h = unit.Height / 2 + precision;
+		y = d * Math.Sin(angle);		
 		double lx = -w, rx = w;
 		double ly = -h, ry = h;
 		return x >= -w && x <= w &&
@@ -1018,7 +1022,7 @@ abstract class ActualStrategy
 		{
 			foreach (var p in bounds)
 			{
-				Unit unit = TestCollision(p.x, p.y, tick, -10, 2, null);
+				Unit unit = TestCollision(p.x, p.y, tick, -1, 2, null);
 				if (unit != null)
 				{
 					resTick = tick;
